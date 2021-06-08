1616ccab16af2424632329200f15337e220f1596
sayanshee-01exe <75190242+sayanshee-01exe@users.noreply.github.com>
2021-06-08 14:58:25 +0530

    ludo game

---
+++ index.js
@@ -0,0 +1,23 @@
+// dice1
+
+var randomNumber1 = Math.floor(Math.random()*6)+1;
+var randomDiceImage1="images/dice"+randomNumber1+".png";
+var image1=document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomDiceImage1);
+
+// dice2
+
+var randomNumber2=Math.floor(Math.random()*6)+1;
+var randomDiceImage2="images/dice"+randomNumber2+".png";
+var image2=document.querySelectorAll("img")[1];
+image2.setAttribute("src", randomDiceImage2);
+
+if(randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML="Player1 wins";
+}
+else if(randomNumber1<randomNumber2){
+    document.querySelector("h1").innerHTML="Player2 wins";
+}
+else{
+    document.querySelector("h1").innerHTML="Draw";
+}
